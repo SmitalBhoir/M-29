1ffbbdba0ec0f8194557cb5c1c05dcd940f17aa7
SmitalBhoir <smitalbhoir77782@gmail.com>
2023-04-12 19:16:49 +0530

    First Commit

---
+++ dayone/src/org/tnsif/introduction/PrimitiveDatatypes.java
@@ -0,0 +1,24 @@
+package org.tnsif.introduction;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		
+		byte num1=-125;
+		//byte =-128 to 127
+		System.out.println(num1);
+		
+		short  num2 =3467;
+		//short -32768 to 32767
+		System.out.println(num2);
+		
+		
+		long num3 =38477443l;
+		System.out.println(num3);
+		
+		char s = 'g';
+		System.out.println("charater  "+s);
+
+	}
+
+}
