fa0613b78eb9e399e55c7e01861c542e53fb3df0
SmitalBhoir <smitalbhoir77782@gmail.com>
2023-05-22 14:02:13 +0530

    First Commit

---
+++ dayten/src/org/tnsif/objectsclassdemo/ObjectClassDemo.java
@@ -0,0 +1,41 @@
+//\program to demonstrate on object class
+package org.tnsif.objectsclassdemo;
+
+class Sample{
+	
+          }
+class Demo
+{
+	//constructor
+	Demo()
+	{
+		//resource allocate
+		System.out.println("Inside constuctor");
+	}
+	protected void finalize()
+	{
+		System.out.println("In Finalize method");
+	}
+	
+}
+
+ 
+public class ObjectClassDemo {
+
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		System.out.println(s.getClass());
+		System.out.println(s.hashCode());
+		
+		Demo d=new Demo();
+		System.out.println(d.getClass());
+		System.out.println(d.hashCode());
+		/*The object class is used when you want to refer
+		 * any object whose data type you don't know */
+		Object obj=new Object();
+		System.out.println(obj.getClass());
+		
+
+	}
+
+}
---
+++ dayten/src/org/tnsif/stringprogram/StringOpeartion.java
@@ -0,0 +1,15 @@
+package org.tnsif.stringprogram;
+
+public class StringOpeartion {
+
+	public static void main(String[] args) {
+		String str1=new String("Ruchi ");
+		System.out.println(str1.toUpperCase());
+		System.out.println(str1.length());
+		System.out.println(str1.lastIndexOf('u'));
+		System.out.println(str1.hashCode());
+		
+
+	}
+
+}
