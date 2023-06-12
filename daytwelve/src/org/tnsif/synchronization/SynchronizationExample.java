928b816b9c43993d889ae6e148443b805df36a98
hp <hp@DESKTOP-77R4VDD>
2023-06-12 14:24:01 +0530

    First Commit

---
+++ daytwelve/src/org/tnsif/synchronization/SynchronizationExample.java
@@ -0,0 +1,14 @@
+package org.tnsif.synchronization;
+
+//driver class
+public class SynchronizationExample {
+
+	public static void main(String[] args) {
+		Power p=new Power();
+		ThreadOne t1=new ThreadOne(p);
+		ThreadTwo t2=new ThreadTwo(p);
+		t1.start();
+		t2.start();
+	}
+
+}
---
+++ daytwelve/src/org/tnsif/synchronization/ThreadOne.java
@@ -0,0 +1,5 @@
+package org.tnsif.synchronization;
+
+public class ThreadOne {
+
+}
