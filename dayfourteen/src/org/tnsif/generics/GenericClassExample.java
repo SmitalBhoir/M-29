5502adffc503052b06e6a442dea50368c7db1935
hp <hp@DESKTOP-77R4VDD>
2023-06-14 12:25:26 +0530

    First Commit

---
+++ dayfourteen/src/org/tnsif/generics/GenericClassExample.java
@@ -0,0 +1,24 @@
+//program t demonstrate on generic class 
+package org.tnsif.generics;
+
+//generics class
+class GenericClass<T>{
+	T x;
+	void print(T x) {
+		this.x=x;
+	}
+	T accept() {
+		return x;
+	}
+}
+//driver class
+public class GenericClassExample {
+
+	public static void main(String[] args) {
+		GenericClass<String> obj = new GenericClass<>();
+		obj.print("Smital");
+		System.out.println(obj.accept());
+
+	}
+
+}
